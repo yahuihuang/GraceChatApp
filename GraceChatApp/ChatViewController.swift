e23cc011f3a46f5d067e443b6afafd55393b1900
Huang Grace <huang.yahui.grace@gmail.com>
2019-12-21 15:10:15 +0800

    add table view and cell

--- GraceChatApp/ChatViewController.swift
+++ GraceChatApp/ChatViewController.swift
@@ -8,23 +8,27 @@
 
 import UIKit
 
-class ChatViewController: UIViewController {
-
+class ChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    //MARK: tableView Delegate
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tableViewCell") as! DiscListTableViewCell
+        cell.title.text = "Design by Grace"
+        return cell
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
---
+++ GraceChatApp/DiscListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DiscListTableViewCell.swift
+//  GraceChatApp
+//
+//  Created by grace on 2019/12/21.
+//  Copyright © 2019 grace. All rights reserved.
+//
+
+import UIKit
+
+class DiscListTableViewCell: UITableViewCell {
+    @IBOutlet weak var title: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
