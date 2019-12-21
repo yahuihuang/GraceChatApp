0c66aac5019d2866112012e4c7c7f88ace6fd464
Huang Grace <huang.yahui.grace@gmail.com>
2019-12-21 15:53:55 +0800

    討論區文章查詢

--- GraceChatApp/ChatViewController.swift
+++ GraceChatApp/ChatViewController.swift
@@ -22,32 +22,52 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
         tableView.dataSource = self
         
         dbRef.child("subject").observeSingleEvent(of: .value) { (snapshot) in
+            self.forumArray.removeAll()
+            
             print(snapshot)
             for snapshotItem in snapshot.children {
                 print(snapshotItem)
                 if let item = snapshotItem as? DataSnapshot {
-                    for snapshotItem2 in item.children {
-                        print(snapshotItem2)
-                        if let item2 = snapshotItem2 as? DataSnapshot {
-                            print("item2=> \(item2.key): \(item2.value as? String ?? "")")
-                        }
-                    }
+                    var subject = item.childSnapshot(forPath: "subject").value as! String
+                    self.forumArray.append(subject)
+//                    for snapshotItem2 in item.children {
+//                        print(snapshotItem2)
+//                        if let item2 = snapshotItem2 as? DataSnapshot {
+//                            print("item2=> \(item2.key): \(item2.value as? String ?? "")")
+//                        }
+//                    }
                 }
             }
+            print(self.forumArray)
+            self.tableView.reloadData()
+        }
+        
+        dbRef.child("subject").observe(.value) { (snapshot) in
+            self.forumArray.removeAll()
+            for snapshotItem in snapshot.children {
+                if let item = snapshotItem as? DataSnapshot {
+                    var subject = item.childSnapshot(forPath: "subject").value as! String
+                    self.forumArray.append(subject)
+                }
+            }
+                        
+            self.tableView.reloadData()
         }
     }
     
     //MARK: tableView Delegate
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
+        return forumArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "tableViewCell") as! DiscListTableViewCell
-        cell.title.text = "Design by Grace"
+        cell.title.text = forumArray[indexPath.row]
         return cell
     }
     
-
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "goDiscList", sender: self)
+    }
 
 }
